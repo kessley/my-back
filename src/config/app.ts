e796420c29d3b93567d8a54cbea756c907734a46
kessley <79649917+kessley@users.noreply.github.com>
2023-03-03 10:34:10 -0300

    cors

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import { setupRoutes } from './router'
 const app = express()
-app.use(cors)
+app.use(cors())
 setupRoutes(app)
 export default app
--- src/controller/RegisterUserController.ts
+++ src/controller/RegisterUserController.ts
@@ -8,7 +8,11 @@ class RegisterUserController {
     
     registerUser =async (request:Request, response: Response) => {
         const {username, email, password} = request.body
-        console.log(username," ",  password," ", email)
+        if (!username) return response.status(400).send("Need to pass a usename valid")
+        if (!email) return response.status(400).send("Need to pass a email valid")
+        if (!password) return response.status(400).send("Need to pass a password valid")
+
+        console.log(username)
     }
 
 }
